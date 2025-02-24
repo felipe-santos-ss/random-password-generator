3e4434c17e74ba0b0267d63db9fc4b4b164f20c5
Felipe <felipesantoss427@gmail.com>
2025-02-24 11:53:22 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789"
+const symbol = "@3$%^&*()_+~|{}[]></-=";
+
+const alChars = upperCase + lowerCase + number + symbol;
+
+function generatePassword(){
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)]
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+  password += number[Math.floor(Math.random() * number.length)]
+  password += symbol[Math.floor(Math.random() * symbol.length)]
+
+  while(lenght > password.length){
+    password += alChars[Math.floor(Math.random() * alChars.length)]
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword(){
+  passwordBox.select();
+  document.execCommand("copy")
+}
